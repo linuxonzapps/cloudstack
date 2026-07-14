93203d85813fdf7b5a02ffe9b451f5d7400a778c
rposts <rishi.investigate@gmail.com>
2026-07-14 19:37:17 +0000

    Add CloudStack processing
    Signed-off-by: rposts <rishi.investigate@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+# Apache CloudStack
+
+Apache CloudStack is open source software designed to deploy and manage large
+networks of virtual machines, as a highly available, highly scalable
+Infrastructure as a Service (IaaS) cloud computing platform. CloudStack is used
+by a number of service providers to offer public cloud services, and by many
+companies to provide an on-premises (private) cloud offering, or as part of a
+hybrid cloud solution.
+
+Additional information available [here](https://github.com/apache/cloudstack).
+
+>**Note:** Artifacts available in this repository are targeted for `s390x` architecture.
---
+++ src/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -o pipefail
+read -ra arr <<< "$@"
+version=${arr[1]}
+trap 0 1 2 ERR
+# Extract DISTRO details for tagging
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO="$ID-$VERSION_ID"
+    if [ "$VERSION_CODENAME" != "" ]; then
+        DISTRO="$ID-$VERSION_CODENAME"
+    fi
+fi
+current_dir="$PWD"
+echo $DISTRO > .distro_zab.txt
+apt update; apt install sudo -y
+# Clone linux-on-ibm-z to keep it current
+git clone https://github.com/linux-on-ibm-z/scripts.git /tmp/linux-on-ibm-z
+# CloudStack build will create *.deb packages
+bash /tmp/linux-on-ibm-z-scripts/CloudStack/${version}/build_cloudstack.sh -y
+tar cvfz cloudstack-${version}-linux-s390x.tar.gz *.deb
+exit 0
